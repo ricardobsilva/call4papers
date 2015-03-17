a30b83ea8867dacdc75a4cf5699de4842567ae16
Gustavo Cunha <gustavocunha@gmail.com>
2015-03-17 14:03:33 -0300

    Fixes on host config

--- config/initializers/environments.rb
+++ config/initializers/environments.rb
@@ -8,3 +8,9 @@ Rails.env.on(:test) do |config|
   config.i18n.available_locales = :en
   config.i18n.default_locale = :en
 end
+
+Rails.env.on(:production) do |config|
+  config.action_mailer.default_url_options = { host: 'lts.alagoasdevday.com.br' }
+  config.i18n.available_locales = ["pt-BR", :en]
+  config.i18n.default_locale = "pt-BR"
+end
